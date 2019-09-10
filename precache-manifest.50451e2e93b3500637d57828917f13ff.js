505d146d798d39331afefa0e23519f2459e57095
GhabiHamza <ghabihamza77@gmail.com>
2019-09-10 16:49:07 +0200

    Updates

---
+++ precache-manifest.50451e2e93b3500637d57828917f13ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba0b42aefa75969b7856795f006d28c",
+    "url": "/movie-app-redux/index.html"
+  },
+  {
+    "revision": "41dce2d4d5863a05780f",
+    "url": "/movie-app-redux/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "26c261d3fd03136e5970",
+    "url": "/movie-app-redux/static/css/main.c95de714.chunk.css"
+  },
+  {
+    "revision": "41dce2d4d5863a05780f",
+    "url": "/movie-app-redux/static/js/2.1dcdec8b.chunk.js"
+  },
+  {
+    "revision": "26c261d3fd03136e5970",
+    "url": "/movie-app-redux/static/js/main.3b0fdbf4.chunk.js"
+  },
+  {
+    "revision": "f5818102e747cf0d6a42",
+    "url": "/movie-app-redux/static/js/runtime~main.965c18cc.js"
+  }
+]);
